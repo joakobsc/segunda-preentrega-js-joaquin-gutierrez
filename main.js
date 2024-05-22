86dca14ac2ff732c5a178421616c99f068f4c5da
Joaquin <joakobsc@gmail.com>
2024-05-22 11:03:47 -0300

    segundo commit

--- main.js
+++ main.js
@@ -14,33 +14,42 @@ let medico6= new Medico ("JOAQUIN","TRAUMATOLOGIA","DOMINGO")
 let medico7= new Medico ("MAXIMILIANO","ENDOCRINOLOGIA","MIERCOLES")
 
 let lista = [medico1,medico2,medico3,medico4,medico5,medico6,medico7]
-let especialidades = ["CARDIOLOGOS", "NEUROLOGOS", "PEDIATAS", "TRAUMATOLOGOS", "ENDOCRINOLOGOS"    ]
+let especialidades = ["CARDIOLOGOS", "NEUROLOGOS", "PEDIATRAS", "TRAUMATOLOGOS", "ENDOCRINOLOGOS"]
 
 especialidades.forEach(
     function(i){
         alert(i.toLowerCase())
     }
 )
-let descripcion = prompt("ingresá la especialidad que buscas o el nombre del profesional").trim().toUpperCase()
-
-if(descripcion = Number || descripcion==""){
-alert("ingresá una especialidad valida por favor")
-descripcion = prompt ("ingresá la especialidad que buscas o el nombre del profesional").trim().toUpperCase()
+function solicitarBusqueda(){
+    while(true){
+        let solicitarEspecialidad=prompt("Ingresá la especialidad que buscas o el nombre del profesional")
+        if(solicitarEspecialidad !== null && solicitarEspecialidad.trim()!=="" && isNaN(solicitarEspecialidad)){
+            return solicitarEspecialidad.trim().toUpperCase()
+        }else{
+            alert("Entrada no valida. Por favor ingresar el dato solicitado nuevamente")
+        }
+    }
 }
+
+let descripcion = solicitarBusqueda();
+
 
 buscaUnProfesional()
 function buscaUnProfesional() {
     
     
-    alert("en la consola puedes observar los especialistas disponibles")
+
     let resultado = lista.filter((medico)=> medico.nombre.toUpperCase().includes(descripcion))
     let resultado2 = lista.filter((medico)=>medico.especialidad.toUpperCase().includes(descripcion))
 
     if(resultado.length > 0){
         console.table (resultado)
+        alert("en la consola puedes observar los especialistas disponibles")
     }
     else if(resultado2.length >0){
         console.table (resultado2)
+        alert("en la consola puedes observar los especialistas disponibles")
     }
     else{
         alert("no encontramos ninguna concidencia con la descripción, lo sentimos")
